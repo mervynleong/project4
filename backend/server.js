943329a7c04f81abd89e763a35a2017379dce587
mervynleong <changpeng66@gmail.com>
2024-07-18 22:15:48 +0800

    app using protection

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,16 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(cors());
+app.use(helmet());
+app.use(limiter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
