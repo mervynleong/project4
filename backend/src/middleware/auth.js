d0d254b7650891121e0a937f66403829f19c76a1
mervynleong <changpeng66@gmail.com>
2024-07-19 17:30:24 +0800

    created authbuyer

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -43,4 +43,28 @@ const authSeller = (req, res, next) => {
   }
 };
 
-module.exports = { authGeneral, authSeller };
+const authBuyer = (req, res, next) => {
+  if (!("authorization" in req.headers)) {
+    return res.status(400).json({ status: "error", msg: "token required" });
+  }
+
+  const token = req.headers["authorization"].replace("Bearer ", "");
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+
+      if (decoded.role === "buyer") {
+        req.decoded = decoded;
+        next();
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      return res.status(403).json({ status: "error", msg: "not authorised" });
+    }
+  } else {
+    return res.status(403).json({ status: "error", msg: "forbidden" });
+  }
+};
+
+module.exports = { authGeneral, authSeller, authBuyer };
