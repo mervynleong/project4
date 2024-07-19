982c6b48bf4d081d3cbb1492b0e88372905b326c
mervynleong <changpeng66@gmail.com>
2024-07-19 17:06:46 +0800

    register

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -2,3 +2,24 @@ const Auth = require("../models/Auth");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { v4: uuidv4 } = require("uuid");
+
+const register = async (req, res) => {
+  try {
+    const authentication = await Auth.findOne({ email: req.body.email });
+    if (authentication) {
+      return res.status(400).json({ status: "error", msg: "duplicate email" });
+    }
+    const hash = await bcrypt.hash(req.body.password, 12);
+    await Auth.create({
+      email: req.body.email,
+      hash,
+      role: req.body.role,
+    });
+    res.json({ status: "ok", msg: "buyer/seller account created" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ status: "error", msg: "invalid registration" });
+  }
+};
+
+module.exports = { register };
