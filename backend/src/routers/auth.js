11d55aa23290c5401ba943c399f138e507707395
mervynleong <changpeng66@gmail.com>
2024-07-19 17:28:56 +0800

    put into routers first for later

--- backend/src/routers/auth.js
+++ backend/src/routers/auth.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { register, login, refresh } = require("../controllers/auth");
 
+const { authGeneral, authSeller } = require("../middleware/auth");
+
 router.put("/reg", register);
 router.post("/login", login);
 router.post("/refresh", refresh);
