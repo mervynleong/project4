244818723fe50360a4e65fc005f311f029746b4b
mervynleong <changpeng66@gmail.com>
2024-07-19 17:17:00 +0800

    login created with routing

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -22,4 +22,43 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  try {
+    const authentication = await Auth.findOne({ email: req.body.email });
+    if (!authentication) {
+      return res
+        .status(401)
+        .json({ status: "error", msg: "user is not authorised" });
+    }
+    const result = await bcrypt.compare(req.body.password, authentication.hash);
+    if (!result) {
+      console.log("email or password error");
+      return res.status(401).json({ status: "error", msg: "login failed" });
+    }
+
+    const claims = {
+      email: authentication.email,
+      role: authentication.role,
+    };
+
+    const access = jwt.sign(claims, process.env.ACCESS_SECRET, {
+      expiresIn: "20m",
+      jwtid: uuidv4(),
+    });
+
+    const refresh = jwt.sign(claims, process.env.REFRESH_SECRET, {
+      expiresIn: "30d",
+      jwtid: uuidv4(),
+    });
+
+    res.json({ access, refresh });
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({
+      status: "error",
+      msg: "login failed",
+    });
+  }
+};
+
+module.exports = { register, login };
