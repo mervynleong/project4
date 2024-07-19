104db32f56592ce1e1c9e4aa89ed767683d3d4f7
mervynleong <changpeng66@gmail.com>
2024-07-19 17:35:29 +0800

    inserting validators

--- backend/src/routers/auth.js
+++ backend/src/routers/auth.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 const { register, login, refresh } = require("../controllers/auth");
 
+const {
+  validateReg,
+  validateLogin,
+  validateRefresh,
+} = require("../validators/auth");
+
+const { checkErrors } = require("../validators/checkErrors");
+
 const { authGeneral, authSeller } = require("../middleware/auth");
 
 router.put("/reg", register);
