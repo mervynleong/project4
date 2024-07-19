6450afabcf0d0f51124d4850cab0b16d75f6dfc7
mervynleong <changpeng66@gmail.com>
2024-07-19 17:27:33 +0800

    middleware for buyer and seller

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,46 @@
+const jwt = require("jsonwebtoken");
+
+const authGeneral = (req, res, next) => {
+  if (!("authorization" in req.headers)) {
+    return res.status(400).json({ status: "error", msg: "token required" });
+  }
+
+  const token = req.headers["authorization"].replace("Bearer ", "");
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+      req.decoded = decoded;
+      next();
+    } catch (error) {
+      return res.status(401).json({ status: "error", msg: "not authorised" });
+    }
+  } else {
+    return res.status(403).json({ status: "error", msg: "forbidden" });
+  }
+};
+
+const authSeller = (req, res, next) => {
+  if (!("authorization" in req.headers)) {
+    return res.status(400).json({ status: "error", msg: "token required" });
+  }
+
+  const token = req.headers["authorization"].replace("Bearer ", "");
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+
+      if (decoded.role === "seller") {
+        req.decoded = decoded;
+        next();
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      return res.status(403).json({ status: "error", msg: "not authorised" });
+    }
+  } else {
+    return res.status(403).json({ status: "error", msg: "forbidden" });
+  }
+};
+
+module.exports = { authGeneral, authSeller };
