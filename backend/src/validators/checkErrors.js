d0964077265f791bae5a1dad2f4f235cc1509384
mervynleong <changpeng66@gmail.com>
2024-07-19 17:31:26 +0800

    check errors

---
+++ backend/src/validators/checkErrors.js
@@ -0,0 +1,13 @@
+const { validateResult } = require("express-validator");
+
+const checkErrors = (req, res, next) => {
+  const errors = validateResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400).json({ status: "error", msg: errors.array() });
+  } else {
+    next();
+  }
+};
+
+module.exports = checkErrors;
