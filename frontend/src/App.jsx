3adf2ebcc941c2918129f2541d8c48208b636d79
mervynleong <changpeng66@gmail.com>
2024-07-24 13:02:28 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,27 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import UserContext from "./context/user";
 import Display from "./components/Display";
-// remember to import login and register
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import NavBar from "./components/NavBar";
+
 const queryClient = new QueryClient();
 
 function App() {
   const [accessToken, setAccessToken] = useState("");
-  // const [role, setRole] = useState("");
   const [showLogin, setShowLogin] = useState(true);
+  const [type, setType] = useState("");
+  const [username, setUsername] = useState("");
 
   return (
     <QueryClientProvider client={queryClient}>
       <UserContext.Provider
-        value={{ accessToken, setAccessToken, role, setRole }}
+        value={{ accessToken, setAccessToken, type, setType }}
       >
+        {/* using accesstoken to set display if accesstoken is true it will display, basically needs login */}
+        <Navbar />
         {!accessToken && showLogin && <Login setShowLogin={setShowLogin} />}
         {!accessToken && !showLogin && <Register setShowLogin={setShowLogin} />}
         {accessToken && <Display />}
