4020dd87463c3e300eca6bc9d5c1befe04041395
mervynleong <changpeng66@gmail.com>
2024-07-25 16:39:33 +0800

    validators for product work

--- backend/src/routers/product.js
+++ backend/src/routers/product.js
@@ -1,14 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// const {
-//   createProduct,
-//   updateProductByID,
-//   deleteProductByID,
-//   getAllProducts,
-//   createProductPG,
-// } = require("../controllers/product");
-
 const {
   createProductPG,
   buyItemPG,
@@ -25,42 +17,48 @@ const {
   authAdminAndSeller,
 } = require("../middleware/auth");
 
+const {
+  validateIdInParam,
+  validateProductData,
+} = require("../validators/product");
+
 const { checkErrors } = require("../validators/checkErrors");
-// const {
-//   validateIdInParam,
-//   validateProductData,
-// } = require("../validators/product");
 
-// router.put(
-//   "/createProduct/:id",
-//   validateIdInParam,
-//   validateProductData,
-//   checkErrors,
-//   authSeller,
-//   createProduct
-// );
-// router.patch(
-//   "/updateProduct/:id",
-//   validateIdInParam,
-//   validateProductData,
-//   checkErrors,
-//   authSeller,
-//   updateProductByID
-// );
-// router.delete(
-//   "/delete/:id",
-//   validateIdInParam,
-//   checkErrors,
-//   authSeller,
-//   deleteProductByID
-// );
-// router.get("/allProducts", authGeneral, getAllProducts);
-
-router.put("/new", authSeller, createProductPG);
-router.patch("/buyItem/:item_uuid", authBuyer, buyItemPG);
-router.delete("/:item_uuid", authAdminAndSeller, deleteItemPG);
-router.patch("/updateItem/:item_uuid", authSeller, updateItemPG);
-router.get("/item/:item_uuid", authGeneral, getItemByIDPG);
+router.put(
+  "/new",
+  validateProductData,
+  checkErrors,
+  authSeller,
+  createProductPG
+);
+router.patch(
+  "/buyItem/:item_uuid",
+  validateIdInParam,
+  checkErrors,
+  authBuyer,
+  buyItemPG
+);
+router.delete(
+  "/:item_uuid",
+  validateIdInParam,
+  checkErrors,
+  authAdminAndSeller,
+  deleteItemPG
+);
+router.patch(
+  "/updateItem/:item_uuid",
+  validateProductData,
+  checkErrors,
+  authSeller,
+  updateItemPG
+);
+router.get(
+  "/item/:item_uuid",
+  validateIdInParam,
+  checkErrors,
+  authGeneral,
+  getItemByIDPG
+);
 router.get("/all", authGeneral, getAllItemPG);
 
 module.exports = router;
--- backend/src/validators/product.js
+++ backend/src/validators/product.js
@@ -1,15 +1,21 @@
 const { body, param } = require("express-validator");
 
-const validateIdInParam = [param("id", "id is invalid").isMongoId()];
+const validateIdInParam = [
+  param("item_uuid", "item_uuid is invalid").notEmpty(),
+];
 
 const validateProductData = [
-  body("itemName", "item Name must not be empty").notEmpty(),
-  body("description", "description must not be empty").notEmpty(),
-  body(
-    "condition",
-    "condition must be one of the following: new, used, like new, mint"
-  ).notEmpty(),
-  body("category", "category must not be empty").notEmpty(),
+  body("sell_price", "sell_price must not be empty and must be numeric")
+    .notEmpty()
+    .isNumeric(),
+  body("description", "description must not be empty").notEmpty().isString(),
+  body("item_name", "item_name must be within 20 characters")
+    .notEmpty()
+    .isString()
+    .isLength({ min: 1, max: 20 }),
+  body("status", "status must not be empty. It must be AVAILABLE")
+    .notEmpty()
+    .isString(),
 ];
 
 module.exports = {
