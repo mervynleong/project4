b39a4191e0609e678d2e69bea922cf5f4a67af36
mervynleong <changpeng66@gmail.com>
2024-07-27 16:50:55 +0800

    doing some modals

---
+++ frontend/src/components/ItemModal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ItemModal = () => {
+  return <div>Hello</div>;
+};
+
+export default ItemModal;
--- frontend/src/components/Marketplace.jsx
+++ frontend/src/components/Marketplace.jsx
@@ -9,6 +9,7 @@ import {
 } from "react";
 import useFetch from "../hooks/useFetch";
 import { useQuery } from "@tanstack/react-query";
+import ItemModal from "./ItemModal";
 
 const Marketplace = () => {
   const queryClient = useQueryClient();
@@ -22,6 +23,7 @@ const Marketplace = () => {
   const [status, setStatus] = useState("");
   const [seller_username, setSeller_username] = useState("");
   const [buyer_username, setBuyer_username] = useState("");
+  const [showItemModal, setShowItemModal] = useState(false);
 
   const { isSuccess, isError, error, isFetching, data } = useQuery({
     queryKey: ["item"],
@@ -34,7 +36,7 @@ const Marketplace = () => {
       ),
   });
 
-  const mutation = useMutation({
+  const { mutate } = useMutation({
     mutationFn: async () =>
       await usingFetch(
         "/product/new",
@@ -78,7 +80,80 @@ const Marketplace = () => {
       setBuy_price(parsedValue);
     }
   };
-  return <div>Hello</div>;
+  return (
+    <>
+      <div className="container">
+        <h2>Items Available</h2>
+        <div className="row">
+          <input
+            type="text"
+            value={item_name}
+            placeholder="Item Name"
+            className="col-md-3"
+            onChange={(event) => setItem_name(event.target.value)}
+          />
+          <input
+            type="text"
+            value={description}
+            placeholder="description"
+            className="col-md-3"
+            onChange={(event) => setDescription(event.target.value)}
+          />
+          <input
+            type="text"
+            value={sell_price}
+            placeholder="sell price"
+            className="col-md-3"
+            onChange={(event) => setSell_price(event.target.value)}
+          />
+          <input
+            type="text"
+            value={sell_price}
+            placeholder="sell price"
+            className="col-md-3"
+            onChange={(event) => setSell_price(event.target.value)}
+          />
+          <input
+            type="text"
+            value={status}
+            placeholder="status"
+            className="col-md-3"
+            onChange={(event) => setStatus(event.target.value)}
+          />
+          <button
+            className="col-md-3"
+            onClick={() => {
+              setShowItemModal(true);
+            }}
+          >
+            Add New Product?
+          </button>
+        </div>
+        <br />
+        <br />
+        <div className="row">
+          <div className="col-md-3">Item Name</div>
+          <div className="col-md-2"></div>
+          <div className="col-md-3">Description</div>
+          <div className="col-md-2"></div>
+          <div className="col-md-3">Sell Price</div>
+          <div className="col-md-2"></div>
+          <div className="col-md-3">Status</div>
+          <div className="col-md-2"></div>
+        </div>
+
+        {showItemModal && (
+          <ItemModal
+            setShowModal={setShowItemModal}
+            item_name={item_name}
+            description={description}
+            sell_price={sell_price}
+            status={status}
+          ></ItemModal>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Marketplace;
