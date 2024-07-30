a909c037585028e33e90667001db9aa131d77977
mervynleong <changpeng66@gmail.com>
2024-07-30 16:47:52 +0800

    made a button on navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -19,6 +19,28 @@ const NavBar = () => {
           <NavLink to="/">HOME</NavLink>
         </li>
       </ul>
+      <li
+        style={{
+          margin: "0",
+          padding: "0",
+          listStyle: "none",
+        }}
+      >
+        <button
+          style={{
+            backgroundColor: "rgb(49, 238, 49)",
+            color: "black",
+            position: "fixed",
+            top: 0,
+            right: 0,
+            margin: "10px",
+          }}
+          className="logout"
+          onClick={() => console.log("hello")}
+        >
+          Logout
+        </button>
+      </li>
     </nav>
   );
 };
